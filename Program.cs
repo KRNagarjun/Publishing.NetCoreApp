bc9646b1730e4d6a911611559b8ffe38cb3a52f7
KRNagarjun <kr.nagarjun@ltts.com>
2021-04-28 11:33:45 +0530

    Publishing app demo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Humanizer;
+
+namespace PublishingAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("109".Humanize());
+            Console.WriteLine(1000009.ToOrdinalWords());
+            Console.WriteLine(1000009.ToWords());
+            Console.WriteLine(1000009.ToRoman());
+            Console.WriteLine(1000009.Tupleize());
+        }
+    }
+}
